5b03e64ea48942d58dcf4cb9335e1fa44da16ee8
byte33 <mltullo1016@gmail.com>
2018-06-13 00:57:35 -0400

    Added Classes and Tested Method

---
+++ src/ecoSchools/EnergyCalculator.java
@@ -0,0 +1,12 @@
+package ecoSchools;
+
+public class EnergyCalculator {
+
+	public static void main(String[] args) {
+		EnergyMethods test = new EnergyMethods();
+		
+		test.helloWorld();
+
+	}
+
+}
---
+++ src/ecoSchools/EnergyMethods.java
@@ -0,0 +1,7 @@
+package ecoSchools;
+
+public class EnergyMethods {
+	void helloWorld() {
+		System.out.println("Hello World!");
+	}
+}
